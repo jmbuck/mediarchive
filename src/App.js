ca6cb94e8bcb8e4e4de0d0391156010911e5c352
Jordan Buckmaster <buckmast@purdue.edu>
2017-11-18 00:05:03 -0500

    Create default route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
+
+import Main from './Main'
+import './App.css'
 
 class App extends Component {
+  
+  signedIn = () => {
+    return true;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/" render={() =>
+            this.signedIn() 
+            ? <Main />
+            : <Redirect to="/sign-in" />
+          }/>
       </div>
     );
   }
