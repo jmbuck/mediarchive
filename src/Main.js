19918f78dc4ac1d095e5e4f878e310d908365835
Jordan Buckmaster <buckmast@purdue.edu>
2017-11-18 00:03:55 -0500

    Add react router to project

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Main.css'
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+        Let there be life
+      </div>
+    );
+  }
+}
+
+export default Main;
