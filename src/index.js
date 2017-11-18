17336000bf76776a4677d0b86d99394ce35dd32a
Jordan Buckmaster <buckmast@purdue.edu>
2017-11-18 18:33:18 -0500

    Add header

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import './css/index.css';
 import App from './js/App';
 import registerServiceWorker from './js/registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
 <Router>
--- src/js/Header.js
+++ src/js/Header.js
@@ -5,14 +5,24 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <div className="nav">
-
-        </div>
-        <div className="search-bar">
-
-        </div>
-        <div className="sign-out">
-
+        <div className="btn-toolbar" role="toolbar" aria-label="toolbar with lists">
+          <div className="nav btn-group mr-2" role="group" aria-label="list links">
+            <button type="button" className="btn btn-primary">Movies</button>
+            <button type="button" className="btn btn-primary">TV Shows</button>
+            <button type="button" className="btn btn-primary">Books</button>
+          </div>
+          <div className="search-bar input-group">
+            <select className="input-group-addon" id="btnGroupAddon">
+              <option value="movies">Movies</option>
+              <option value="tv">TV Shows</option>
+              <option value="books">Books</option>
+            </select>
+            <input type="text" className="form-control" placeholder="Find media" aria-describedby="btnGroupAddon" />
+          </div>
+          <div className="misc-buttons btn-group" role="group" aria-label="sign out">
+            <button type="button" className="btn btn-secondary">Statistics</button>
+            <button type="button" className="btn btn-secondary">Sign out</button>
+          </div>
         </div>
       </div>
     );
