1261ac3ba0d592d497b305c8b530fe3d01ab343b
Jordan Buckmaster <buckmast@purdue.edu>
2017-11-18 18:46:18 -0500

    Add footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -5,7 +5,10 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-
+        <span className="date">2017</span>
+        <span className="creators">Mediarchive was created by Jordan Buckmaster and Shawn Montgomery</span>
+        <span className="apis">Created using the TMDB and Google Books APIs</span>
+        <span className="repo">Find the repository <a href="https://github.com/jmbuck/mediarchive">here</a></span>
       </div>
     );
   }
