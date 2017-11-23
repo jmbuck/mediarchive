67b86694f43f3d8ab772ca90673c305690aa55d1
Jordan Buckmaster <buckmast@purdue.edu>
2017-11-22 18:41:25 -0600

    Display movie search results in a grid

--- client/src/js/Movie.js
+++ client/src/js/Movie.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
 import '../css/Movie.css'
+import { TMDBKey } from '../keys'
 
 class Movie extends Component {
-  
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      movie: this.props.movie,
+      fetched: false,
+    }
+  }
+
   fetchMovieInfo = (movie) => {
-   
+    fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${TMDBKey}&append_to_response=credits`)
+      .then(response => response.json())
+      .then(detailedMovie => {
+        this.setState({ 
+          movie: detailedMovie,  
+          fetched: true, 
+        })
+      })
+  }
+
+  showMovieInfo = () => {
+
   }
 
   renderMovieInfo = (navProps, movie) => {
@@ -17,11 +37,19 @@ class Movie extends Component {
     const media = this.props.match.params.media
     const query = this.props.match.params.query
     const page = this.props.match.params.page
-    const movie = this.props.movie
+    const movie = {...this.state.movie}
+    const path = `https://image.tmdb.org/t/p/w185${movie.poster_path}`
+
     return (
       <li className="Movie">
-        {movie.title}
-
+        <div className="preview" onClick={this.showMovieInfo}>
+          <div className="title" title={movie.title}>{movie.title}</div>
+          {/*Displays movie poster. If poster does not exist, show "poster does not exist" image*/
+            movie.poster_path 
+            ? <img src={path} alt="movie poster" />
+            : <img src="http://static01.mediaite.com/med/wp-content/uploads/gallery/possilbe-movie-pitches-culled-from-the-mediaite-comments-section/poster-not-available1.jpg" alt="movie poster" />
+          }
+        </div>
         { this.props.search 
           ? <Route path={`/search/movies/${query}/${page}/${movie.id}`} render={(navProps) => {
               this.fetchMovieInfo(movie)
